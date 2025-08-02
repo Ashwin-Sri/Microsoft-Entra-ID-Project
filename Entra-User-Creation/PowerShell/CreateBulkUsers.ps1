1cf42d4c08b14cf559b6ea1428aa16bfb77294c4
Ashwin Srinivasan <ashwin.sri.info@gmail.com>
2025-08-02 18:20:11 +0530

    Update CreateBulkUsers.ps1

--- Entra-User-Creation/PowerShell/CreateBulkUsers.ps1
+++ Entra-User-Creation/PowerShell/CreateBulkUsers.ps1
@@ -1,27 +1,18 @@
-# Connect to Microsoft Graph with required permission
 Connect-MgGraph -Scopes "User.ReadWrite.All"
 
-# Import users from the CSV file
-$users = Import-Csv -Path "C:\Users\Desktop\ENTRA\02-User-Creation-PowerShell\Bulk users creations.csv"
+$users = Import-Csv -Path "C:\Users\Desktop\ENTRA\02-User-Creation-PowerShell\bulk users creations.csv"
 
-# Loop through each user and create the account
 foreach ($user in $users) {
     $body = @{
         accountEnabled = $true
-        displayName = $user.displayName
-        mailNickname = $user.mailNickname
-        userPrincipalName = $user.userPrincipalName
+        displayName = $user.DisplayName
+        mailNickname = $user.MailNickname
+        userPrincipalName = $user.UserPrincipalName
         passwordProfile = @{
-            password = $user.password
+            password = $user.Password
             forceChangePasswordNextSignIn = $true
         }
     }
 
-    try {
-        New-MgUser -BodyParameter $body
-        Write-Host "Created: $($user.userPrincipalName)" -ForegroundColor Green
-    }
-    catch {
-        Write-Host "Error creating $($user.userPrincipalName): $_" -ForegroundColor Red
-    }
+    New-MgUser -BodyParameter $body
 }
