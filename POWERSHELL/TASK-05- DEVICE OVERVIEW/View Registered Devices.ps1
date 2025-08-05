9079b3f1634b1274c4954169994e91cfed17d38f
Ashwin Srinivasan <ashwin.sri.info@gmail.com>
2025-08-05 18:10:41 +0530

    Update View Registered Devices.ps1

--- POWERSHELL/TASK-05- DEVICE OVERVIEW/View Registered Devices.ps1
+++ POWERSHELL/TASK-05- DEVICE OVERVIEW/View Registered Devices.ps1
@@ -1,4 +1,5 @@
 # Connect to Microsoft Graph
+
 Connect-MgGraph -Scopes "Device.Read.All", "Directory.Read.All"
 
 # Retrieve all registered devices
@@ -6,3 +7,8 @@ $devices = Get-MgDevice -All
 
 # Display relevant details
 $devices | Select-Object DisplayName, OperatingSystem, DeviceId, TrustType, IsCompliant, IsEnabled
+
+Optional: Export Device List to CSV
+
+$devices | Select-Object DisplayName, OperatingSystem, DeviceId, TrustType, IsCompliant, IsEnabled |
+Export-Csv -Path "C:\Entra-Registered-Devices.csv" -NoTypeInformation
